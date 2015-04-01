d4eb8ec89f80f506b1eb04d43d869837738fc61d
Kashish Gupta <kashishgupta1990@yahoo.com>
2015-04-01 13:08:00 +0530

    asd

---
+++ Async/0_syncVSasyc.js
@@ -0,0 +1,11 @@
+/*Example of Sync Code*/
+/*console.log('First');
+console.log('Second');
+console.log('Third');*/
+
+/*Example of Async*/
+console.log('First');
+setTimeout(function(){
+    console.log('Second');
+},0);
+console.log('Third');
